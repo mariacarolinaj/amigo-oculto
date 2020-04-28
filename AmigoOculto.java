258a87e57f10d971b29a79656cd7e4edfbcedcbb
Maria Carolina <mariacarolina_bh@hotmail.com>
2020-04-28 14:38:37 -0300

    Inclui assinatura do trabalho

--- AmigoOculto.java
+++ AmigoOculto.java
@@ -1,3 +1,10 @@
+/*
+ * AMIGO OCULTO
+ * Trabalho prático para a disciplina de Algoritmos e Estruturas de Dados III do curso de Ciência da Computação
+ * Implementado por Maria Carolina, Viktor Guilherme e Roberto Mafra em 1/2020
+ * PUC Minas - Professor Marcos Kutova
+ */
+
 import java.io.*;
 import cruds.*;
 
